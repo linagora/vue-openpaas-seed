f22bdf37c69fc732cf2a9002425ec0adaeb1b433
Christophe HAMERLING <chamerling@linagora.com>
2019-07-08 15:20:22 +0200

    Add SearchHeader component

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,12 @@
       </v-navigation-drawer>
       <v-toolbar clipped-left app fixed color="blue" dark v-if="$auth.check()">
         <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-title style="max-width: 300px" class="ml-0 pl-3 hidden-sm-and-down">
           <img class="hidden-sm-and-down" id="header-logo" src="@/assets/logo.svg"/>
         </v-toolbar-title>
-        <v-spacer></v-spacer>
+        <div id="search">
+          <op-search-header/>
+        </div>
         <!--<op-applications-menu/>-->
         <op-user-menu/>
       </v-toolbar>
@@ -33,6 +35,7 @@
 
 <script>
 import { theme } from "@/style";
+import SearchHeader from "@/components/SearchHeader.vue";
 import UserMenu from "@/components/UserMenu.vue";
 import Snackbar from "@/components/Snackbar.vue";
 
@@ -46,7 +49,8 @@ export default {
   },
   components: {
     "op-user-menu": UserMenu,
-    "op-snackbar": Snackbar
+    "op-snackbar": Snackbar,
+    "op-search-header": SearchHeader
   },
   created() {
     this.$auth.ready(() => {
@@ -60,4 +64,9 @@ export default {
   #header-logo
     height: 35px;
     width: 150px;
+
+  #search
+    display: flex
+    flex-grow: 1
+    justify-content: center
 </style>
